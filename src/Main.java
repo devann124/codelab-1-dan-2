e07e542ca1a10dc693fadf3f182710a5b0d1102f
devann124 <devankasyafani2@gmail.com>
2025-10-22 16:49:42 +0700

    modul 3

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        double panjang = 8.5;
+        double lebar = 4.2;
+
+        double luas = panjang * lebar;
+
+        System.out.println("=== Program Menghitung Luas Persegi Panjang ===");
+        System.out.println("Panjang : " + panjang);
+        System.out.println("Lebar   : " + lebar);
+        System.out.println("Luas    : " + luas);
+    }
+}
